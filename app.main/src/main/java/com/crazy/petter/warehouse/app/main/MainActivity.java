21a503b01713bbaf178c3dab63529be04ccf83e0
陈柳柳 <chenliuliu123@126.com>
2017-02-08 17:37:48 +0800

    测试提交

--- app.main/src/main/java/com/crazy/petter/warehouse/app/main/MainActivity.java
+++ app.main/src/main/java/com/crazy/petter/warehouse/app/main/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ToastUtils.showShort(this, "欢迎使用");
+        ToastUtils.showShort(this, "欢迎你使用");
     }
 }
