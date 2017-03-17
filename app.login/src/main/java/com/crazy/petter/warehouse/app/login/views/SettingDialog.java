fc0b83f3e89fe4ad7b7e8047d35be12bace2f869
陈柳柳 <chenliuliu123@126.com>
2017-03-17 16:43:51 +0800

    setting界面修改

--- app.login/src/main/java/com/crazy/petter/warehouse/app/login/views/SettingDialog.java
+++ app.login/src/main/java/com/crazy/petter/warehouse/app/login/views/SettingDialog.java
@@ -3,16 +3,19 @@ package com.crazy.petter.warehouse.app.login.views;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.StyleRes;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ScrollView;
 
 import com.bjdv.lib.utils.constants.Constant;
 import com.bjdv.lib.utils.util.SharedPreferencesUtil;
 import com.bjdv.lib.utils.util.ToastUtils;
+import com.bjdv.lib.utils.widgets.CustomHorizontalScrollView;
 import com.crazy.petter.warehouse.app.login.R;
 
 /**
@@ -24,6 +27,7 @@ public class SettingDialog extends Dialog {
     private SharedPreferencesUtil sp;
     private EditText mEditText;
     private Button btnCommit, btnCancle;
+    CustomHorizontalScrollView mCustomHorizontalScrollView;
 
     public SettingDialog(@NonNull Context context, @StyleRes int themeResId) {
         super(context, themeResId);
@@ -37,6 +41,7 @@ public class SettingDialog extends Dialog {
         mEditText = (EditText) findViewById(R.id.edt_url);
         btnCommit = (Button) findViewById(R.id.btn_commit);
         btnCancle = (Button) findViewById(R.id.btn_cancle);
+        mCustomHorizontalScrollView = (CustomHorizontalScrollView) findViewById(R.id.hsv);
         if (TextUtils.isEmpty(sp.getString("baseUrl"))) {
             mEditText.setText(Constant.SERVER_URL_BASE);
         } else {
@@ -62,5 +67,11 @@ public class SettingDialog extends Dialog {
                 SettingDialog.this.dismiss();
             }
         });
+        new Handler().postDelayed(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mCustomHorizontalScrollView.fullScroll(ScrollView.FOCUS_UP);
+            }
+        }), 150);
     }
 }
